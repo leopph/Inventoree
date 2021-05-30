22d5ca783272c3e147baa87d3db724708e9dd5e5
leopph69 <levente.loffler99@gmail.com>
2021-05-30 17:14:19 +0200

    Improve AddEditActivity GUI

--- app/src/main/java/hu/leopph/inventoree/AddEditProductActivity.kt
+++ app/src/main/java/hu/leopph/inventoree/AddEditProductActivity.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import android.text.format.DateFormat
 import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.Timestamp
@@ -31,8 +32,8 @@ class AddEditProductActivity : AppCompatActivity() {
         val calendar = Calendar.getInstance()
         calendar.set(year, month, day)
 
-        mBinding.startdateEdittext.text.clear()
-        mBinding.startdateEdittext.text.insert(
+        mBinding.prodStartdateEntry.editText?.text?.clear()
+        mBinding.prodStartdateEntry.editText?.text?.insert(
             0,
             DateFormat.getDateFormat(applicationContext)
                 .format(calendar.time)
@@ -43,8 +44,8 @@ class AddEditProductActivity : AppCompatActivity() {
         val calendar = Calendar.getInstance()
         calendar.set(year, month, day)
 
-        mBinding.orderdateEdittext.text.clear()
-        mBinding.orderdateEdittext.text.insert(
+        mBinding.prodOrderdateEntry.editText?.text?.clear()
+        mBinding.prodOrderdateEntry.editText?.text?.insert(
             0,
             DateFormat.getDateFormat(applicationContext)
                 .format(calendar.time)
@@ -55,8 +56,8 @@ class AddEditProductActivity : AppCompatActivity() {
         val calendar = Calendar.getInstance()
         calendar.set(year, month, day)
 
-        mBinding.termdateEdittext.text.clear()
-        mBinding.termdateEdittext.text.insert(
+        mBinding.prodTermdateEntry.editText?.text?.clear()
+        mBinding.prodTermdateEntry.editText?.text?.insert(
             0,
             DateFormat.getDateFormat(applicationContext)
                 .format(calendar.time)
@@ -75,7 +76,7 @@ class AddEditProductActivity : AppCompatActivity() {
 
         val product = intent.getParcelableExtra<Product>("product")!!
 
-        mBinding.startdateEdittext.setOnClickListener {
+        mBinding.prodStartdateEntry.editText?.setOnClickListener {
             val calendar = Calendar.getInstance()
             calendar.time = product.startDate.toDate()
 
@@ -88,7 +89,7 @@ class AddEditProductActivity : AppCompatActivity() {
                 .show()
         }
 
-        mBinding.orderdateEdittext.setOnClickListener {
+        mBinding.prodOrderdateEntry.editText?.setOnClickListener {
             val calendar = Calendar.getInstance()
             calendar.time = product.orderDate.toDate()
 
@@ -101,7 +102,7 @@ class AddEditProductActivity : AppCompatActivity() {
                 .show()
         }
 
-        mBinding.termdateEdittext.setOnClickListener {
+        mBinding.prodTermdateEntry.editText?.setOnClickListener {
             val calendar = Calendar.getInstance()
             calendar.time = product.terminationDate.toDate()
 
@@ -114,35 +115,33 @@ class AddEditProductActivity : AppCompatActivity() {
                 .show()
         }
 
-        mBinding.prodNameEdittext.text.insert(0, product.name)
-        mBinding.prodDescEdittext.text.insert(0, product.description)
-        mBinding.prodSerialEdittext.text.insert(0, product.productSerialNumber)
+        mBinding.prodNameEntry.editText?.text?.insert(0, product.name)
+        mBinding.prodDescEntry.editText?.text?.insert(0, product.description)
+        mBinding.prodSerialEntry.editText?.text?.insert(0, product.productSerialNumber)
         mBinding.prodBundleToggle.isChecked = product.isBundle
-        mBinding.orderdateEdittext.text.insert(0, DateFormat.getDateFormat(applicationContext).format(product.orderDate.toDate()))
-        mBinding.startdateEdittext.text.insert(0, DateFormat.getDateFormat(applicationContext).format(product.startDate.toDate()))
-        mBinding.termdateEdittext.text.insert(0, DateFormat.getDateFormat(applicationContext).format(product.terminationDate.toDate()))
+        mBinding.prodOrderdateEntry.editText?.text?.insert(0, DateFormat.getDateFormat(applicationContext).format(product.orderDate.toDate()))
+        mBinding.prodStartdateEntry.editText?.text?.insert(0, DateFormat.getDateFormat(applicationContext).format(product.startDate.toDate()))
+        mBinding.prodTermdateEntry.editText?.text?.insert(0, DateFormat.getDateFormat(applicationContext).format(product.terminationDate.toDate()))
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, ProductStatusType.values())
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        mBinding.prodStatusSpinner.adapter = adapter
-        mBinding.prodStatusSpinner.setSelection(product.status.ordinal)
-        mBinding.dutyFreeAmountEdittext.text.insert(0, product.price.dutyFreeAmount.value.toString())
-        mBinding.dutyFreeCurrencyEdittext.text.insert(0, product.price.dutyFreeAmount.unit.currencyCode)
-        mBinding.taxIncludedAmountEdittext.text.insert(0, product.price.taxIncludedAmount.value.toString())
-        mBinding.taxIncludedCurrencyEdittext.text.insert(0, product.price.taxIncludedAmount.unit.currencyCode)
-        mBinding.taxrateEdittext.text.insert(0, product.price.taxRate.toString())
+        (mBinding.prodStatusDropdown.editText as? AutoCompleteTextView)?.setAdapter(adapter) // TODO set default
+        mBinding.prodDfreeAmountEntry.editText?.text?.insert(0, product.price.dutyFreeAmount.value.toString())
+        mBinding.prodDfreeCurrEntry.editText?.text?.insert(0, product.price.dutyFreeAmount.unit.currencyCode)
+        mBinding.prodTaxIncAmountEntry.editText?.text?.insert(0, product.price.taxIncludedAmount.value.toString())
+        mBinding.prodTaxincCurrEntry.editText?.text?.insert(0, product.price.taxIncludedAmount.unit.currencyCode)
+        mBinding.prodTaxrateEntry.editText?.text?.insert(0, product.price.taxRate.toString())
     }
 
 
     fun onSavePressed(@Suppress("UNUSED_PARAMETER") view: View) {
         // CHECK FOR EMPTY PRODUCT NAME
-        val productName = mBinding.prodNameEdittext.text.toString()
+        val productName = mBinding.prodNameEntry.editText?.text.toString()
         if (productName.isBlank()) {
             Toast.makeText(this, R.string.bad_prod_name_error, Toast.LENGTH_LONG).show()
             return
         }
 
         // CHECK FOR EMPTY SERIAL NUMBER
-        val serialNumber = mBinding.prodSerialEdittext.text.toString()
+        val serialNumber = mBinding.prodSerialEntry.editText?.text.toString()
         if (serialNumber.isBlank()) {
             Toast.makeText(this, R.string.bad_serial_error, Toast.LENGTH_LONG).show()
             return
@@ -151,7 +150,7 @@ class AddEditProductActivity : AppCompatActivity() {
         // CHECK FOR VALID NUMBER
         val dutyFreeAmount: Float
         try {
-            dutyFreeAmount = mBinding.dutyFreeAmountEdittext.text.toString().toFloat()
+            dutyFreeAmount = mBinding.prodDfreeAmountEntry.editText?.text.toString().toFloat()
         }
         catch (exception: NumberFormatException) {
             // ERROR
@@ -161,7 +160,7 @@ class AddEditProductActivity : AppCompatActivity() {
         // CHECK FOR VALID CURRENCY
         val dutyFreeCurrency: Currency
         try {
-            dutyFreeCurrency = Currency.getInstance(mBinding.dutyFreeCurrencyEdittext.text.toString())
+            dutyFreeCurrency = Currency.getInstance(mBinding.prodDfreeCurrEntry.editText?.text.toString())
         }
         catch (exception: IllegalArgumentException) {
             // ERROR
@@ -171,7 +170,7 @@ class AddEditProductActivity : AppCompatActivity() {
         // CHECK FOR VALID NUMBER
         val taxIncludedAmount: Float
         try {
-            taxIncludedAmount = mBinding.taxIncludedAmountEdittext.text.toString().toFloat()
+            taxIncludedAmount = mBinding.prodTaxIncAmountEntry.editText?.text.toString().toFloat()
         }
         catch (exception: NumberFormatException) {
             // ERROR
@@ -181,7 +180,7 @@ class AddEditProductActivity : AppCompatActivity() {
         // CHECK FOR VALID CURRENCY
         val taxIncludedCurrency: Currency
         try {
-            taxIncludedCurrency = Currency.getInstance(mBinding.taxIncludedCurrencyEdittext.text.toString())
+            taxIncludedCurrency = Currency.getInstance(mBinding.prodTaxincCurrEntry.editText?.text.toString())
         }
         catch (exception: IllegalArgumentException) {
             // ERROR
@@ -191,7 +190,7 @@ class AddEditProductActivity : AppCompatActivity() {
         // CHECK FOR VALID NUMBER
         val taxRate: Float
         try {
-            taxRate = mBinding.taxrateEdittext.text.toString().toFloat()
+            taxRate = mBinding.prodTaxrateEntry.editText?.text.toString().toFloat()
         }
         catch (exception: NumberFormatException) {
             // ERROR
@@ -201,30 +200,30 @@ class AddEditProductActivity : AppCompatActivity() {
         // SET PRODUCT VALUES
         val product = intent.getParcelableExtra<Product>("product")!!
         product.name = productName
-        product.description = mBinding.prodDescEdittext.text.toString()
+        product.description = mBinding.prodDescEntry.editText?.text.toString()
         product.isBundle = mBinding.prodBundleToggle.isChecked
         product.productSerialNumber = serialNumber
         product.price.dutyFreeAmount.unit = dutyFreeCurrency
         product.price.dutyFreeAmount.value = dutyFreeAmount
         product.price.taxIncludedAmount.unit = taxIncludedCurrency
         product.price.taxIncludedAmount.value = taxIncludedAmount
-        product.status = ProductStatusType.valueOf(mBinding.prodStatusSpinner.selectedItem.toString())
+        product.status = ProductStatusType.valueOf((mBinding.prodStatusDropdown.editText as? AutoCompleteTextView)?.text.toString())
         product.price.taxRate = taxRate
 
         product.orderDate = Timestamp(
             java.text.DateFormat.getDateInstance(
                 java.text.DateFormat.SHORT, Locale.getDefault())
-                .parse(mBinding.orderdateEdittext.text.toString())!!)
+                .parse(mBinding.prodOrderdateEntry.editText?.text.toString())!!)
 
         product.startDate = Timestamp(
             java.text.DateFormat.getDateInstance(
                 java.text.DateFormat.SHORT, Locale.getDefault())
-                .parse(mBinding.startdateEdittext.text.toString())!!)
+                .parse(mBinding.prodStartdateEntry.editText?.text.toString())!!)
 
         product.terminationDate = Timestamp(
             java.text.DateFormat.getDateInstance(
                 java.text.DateFormat.SHORT, Locale.getDefault())
-                .parse(mBinding.termdateEdittext.text.toString())!!)
+                .parse(mBinding.prodTermdateEntry.editText?.text.toString())!!)
 
 
         // GO BACK TO LISTING
